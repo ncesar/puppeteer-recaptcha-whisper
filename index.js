18bf753b8f9187a37ed57edf42c0379ad374a308
César Nascimento <ceesar90@gmail.com>
2023-06-21 17:36:45 -0300

    adding gif and new code working properly

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 const axios = require('axios');
+const fs = require('fs');
+const request = require('request');
 
 async function runPuppeteer() {
   const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
 
   // Navigate to the reCAPTCHA API Demo page
   await page.goto('https://www.google.com/recaptcha/api2/demo');
   console.log('Going to captcha page');
   await page.waitForTimeout(4000);
   console.log('Page loaded');
-  page.on('console', (msg) => {
-    console.log(msg.text());
-  });
 
   const frame = await page.frames().find((f) => f.name().startsWith('a-'));
   await frame.waitForSelector('div.recaptcha-checkbox-border');
   //click on checkbox to activate recaptcha
   await frame.click('div.recaptcha-checkbox-border');
   console.log('Captcha exists!');
-  // wait for selector below
-  console.log('Waiting for audio button');
-  await frame.waitForSelector('.rc-button-audio');
-  console.log('Audio button exists, clicking');
-  await frame.click('#recaptcha-audio-button');
-  console.log('Waiting for download link selector');
-  await frame.waitForSelector('.rc-audiochallenge-tdownload-link');
-  console.log('Download button exists');
-  const audioUrl = await frame.evaluate(() => {
+
+  // Switch to the new iframe for the challenge
+  await page.waitForTimeout(3000); // Wait for new iframe to load
+
+  // Find the iframe by its XPath
+  const iframeElementHandle = await page.$x(
+    ".//iframe[@title='recaptcha challenge expires in two minutes']",
+  );
+
+  // Get the frame from the element handle
+  const secondaryIframe = await iframeElementHandle[0].contentFrame();
+  await secondaryIframe.waitForSelector('.rc-button-audio');
+  await secondaryIframe.click('.rc-button-audio');
+  console.log('Audio button clicked');
+  await secondaryIframe.waitForSelector('.rc-audiochallenge-tdownload-link');
+  const audioUrl = await secondaryIframe.evaluate(() => {
     let downloadLink = document.querySelector(
       '.rc-audiochallenge-tdownload-link',
     );
@@ -37,22 +44,45 @@ async function runPuppeteer() {
 
   const response = await axios.get(audioUrl, { responseType: 'arraybuffer' });
   const audioData = Buffer.from(response.data, 'binary');
-  fs.writeFileSync('./audio.mp3', audioData);
+  fs.writeFileSync('./audio.wav', audioData);
 
-  const openaiResponse = await axios.post(
-    'https://api.openai.com/v1/engines/davinci-codex/completions',
-    {
-      audioData: './audio.mp3',
-      headers: {
-        'Content-Type': 'audio/mp3',
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+  // Send the audio file to OpenAI's Whisper API
+
+  const formData = {
+    model: 'whisper-1',
+    file: {
+      value: fs.createReadStream('./audio.wav'),
+      options: {
+        filename: 'audio.wav',
+        contentType: 'audio/wav',
       },
     },
-  );
+  };
 
-  console.log(openaiResponse.data);
-  const text = openaiResponse.data.choices[0].text;
-  console.log(text, 'text');
+  const options = {
+    method: 'POST',
+    url: 'https://api.openai.com/v1/audio/transcriptions',
+    headers: {
+      Authorization: `Bearer ${process.env.OPENAI_KEY}`,
+    },
+    formData: formData,
+  };
+  let audioText = '';
+
+  request(options, async function (error, response, body) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(body, 'body');
+      audioText = JSON.parse(body).text;
+      console.log(audioText, 'audio text');
+      await secondaryIframe.waitForSelector('#audio-response');
+      await secondaryIframe.type('#audio-response', audioText, 500);
+      await secondaryIframe.click('#recaptcha-verify-button');
+    }
+  });
+
+  await page.waitForTimeout(15000);
   await browser.close();
 }
 
